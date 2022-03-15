6bfc8b65a9ef9c2c9ddad6170bc7c5b35dc47f5c
Artem Burdukov <artem_burdukov@epam.com>
2022-03-15 17:29:14 +0200

    add custom attribute for status validation.

---
+++ AdoNetApp.WebApp/Attributes/StringRangeAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AdoNetApp.WebApp.Attributes
+{
+	public class StringRangeAttribute : ValidationAttribute
+	{
+		public Type EnumType { get; set; }
+
+		protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+		{
+			if (Enum.IsDefined(EnumType, value.ToString()))
+			{
+				return ValidationResult.Success;
+			}
+
+			return new ValidationResult("Wrong value for enum.");
+		}
+	}
+}
