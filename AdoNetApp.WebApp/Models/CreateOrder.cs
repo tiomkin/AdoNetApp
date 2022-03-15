a582911cd07899022389e822156a9e1adb4ec9d5
Artem Burdukov <artem_burdukov@epam.com>
2022-03-15 13:07:37 +0200

    add CreateOrder model.

---
+++ AdoNetApp.WebApp/Models/CreateOrder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AdoNetApp.WebApp.Models
+{
+	public class CreateOrder
+	{
+
+		[Required]
+		[StringLength(50)]
+		public string Status { get; set; }
+
+		public int ProductId { get; set; }
+
+	}
+}
